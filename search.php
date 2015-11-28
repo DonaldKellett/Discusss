b366ae59b99b6bb254e5a973e31324b581e2a228
DonaldKellett <i.donaldl@me.com>
2015-11-28 15:34:47 +0800

    Generalise Code in search.php
    I almost totally forgot to generalize the code in the searching script
    so other people can use it.

--- search.php
+++ search.php
@@ -1,1 +1,15 @@
-<?php header("Location: http://google.com.hk/search?q=" . $_GET['q'] . " site:naitsabes.com/discussion_corner/"); die(); ?>
+<?php
+/*
+  search.php
+  Discusss
+  (c) Donald Leung.  All rights reserved.
+  MIT Licensed
+*/
+
+# Note from creator of Discusss: feel free to edit this file however you like UNLESS you have no clue as to how PHP works
+
+# Variables - feel free to edit
+$my_website_no_http = "yourdomain.com"; // WARNING!!!  Do NOT add the http protocol in front of your website domain.
+
+header("Location: http://google.com.hk/search?q=" . $_GET['q'] . " site:" . $my_website_no_http); die();
+?>
